a0be505423425dc0184786a55b2717462824f1e7
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-17 16:17:45 +0200

    solution dice exercise

--- 02-dadi/script.js
+++ 02-dadi/script.js
@@ -7,3 +7,14 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
  */
 
 // ! SOLUZIONE
+const computerNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Il numero del computer e':", computerNumber);
+
+const userNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Il numero del giocatore e':", userNumber);
+
+if (computerNumber > userNumber) {
+  console.log("SCONFITTA! Il computer ha vinto");
+} else {
+  console.log("VITTORIA! Il giocatore ha vinto");
+}
